121cd91f71d2da9f149bc4b4d2b9c2d9ef67cd5c
Amanpreet Singh <apsdehal@gmail.com>
2018-02-26 02:01:18 -0500

    Add logger class

---
+++ modules/logger.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+class Logger:
+    # Implement filewrite later
+    def __init__(self):
+        return
+
+    def write(self, x):
+        print(x)
+
+    def write_new_line(self):
+        self.write(" ")
