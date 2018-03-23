dd60a2efa99d4f277aab101b9950b8375b1e7f9d
Amanpreet Singh <apsdehal@gmail.com>
2018-03-23 15:56:02 -0400

    Add lm utils

---
+++ acceptability/utils/lm.py
@@ -0,0 +1,25 @@
+from torch.autograd import Variable
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Variables, to detach them from their history."""
+    if type(h) == Variable:
+        return Variable(h.data)
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+def batchify(data, bsz, args):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    if args.gpu:
+        data = data.cuda()
+    return data
+
+def get_batch(source, i, seq_len, evaluation=False):
+    seq_len = min(seq_len, len(source) - 1 - i)
+    data = Variable(source[i:i+seq_len], volatile=evaluation)
+    target = Variable(source[i+1:i+1+seq_len].view(-1))
+    return data, target
