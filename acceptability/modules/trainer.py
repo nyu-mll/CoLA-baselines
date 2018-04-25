3fcdb8f5e56d2643698e2d50914bb06bfdaa2781
Alex Warstadt <alexwarstadt@gmail.com>
2018-04-25 12:31:44 -0400

    allow for checkpoint without validating

--- acceptability/modules/trainer.py
+++ acceptability/modules/trainer.py
@@ -183,14 +183,17 @@ class Trainer:
                 break
 
         self.checkpoint.restore()
-        acc, loss, matthews, confusion = self.validate(self.test_loader)
-        other_metrics = {
-            'acc': acc,
-            'val_loss': loss,
-            'confusion_matrix': confusion
-        }
-        self.writer.write("Test Set:")
-        self.print_current_info(0, 0, matthews, other_metrics)
+        try:
+            acc, loss, matthews, confusion = self.validate(self.test_loader)
+            other_metrics = {
+                'acc': acc,
+                'val_loss': loss,
+                'confusion_matrix': confusion
+            }
+            self.writer.write("Test Set:")
+            self.print_current_info(0, 0, matthews, other_metrics)
+        except _:
+            pass
         self.checkpoint.finalize()
 
     def validate(self, loader: torchtext.data.Iterator):
