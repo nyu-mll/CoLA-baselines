4e38c018001253f1059d0fcfb0582c349efbeac6
Alex Warstadt <alexwarstadt@gmail.com>
2018-04-28 20:40:03 -0400

    changed sweep space

--- acceptability/generate_sweep.py
+++ acceptability/generate_sweep.py
@@ -99,9 +99,9 @@ space = {
     'classifier': hp.choice('classifier', [{
             'hidden_size': hp.uniform('hidden_size', 20, 100),
             'embedding_size': hp.uniform('embedding_size', 200, 600),
-            'learning_rate': hp.uniform('learning_rate', -5, -3),
-            'num_layers': hp.uniform('num_layers', 1, 1),
-            'encoding_size': hp.uniform('encoding_size', 300, 1200),
+            'learning_rate': hp.uniform('learning_rate', -5, -3.5),
+            'num_layers': hp.uniform('num_layers', 2, 5 ),
+            'encoding_size': hp.uniform('encoding_size', 600, 1200),
             'encoder_num_layers': hp.uniform('encoder_num_layers', 1, 5),
             'dropout': hp.choice('dropout', [0.2, 0.5])
     }])
