56db28c0411f21f80549dbbff0bd4b4ea1770fc8
Alex Warstadt <alexwarstadt@gmail.com>
2018-11-08 21:03:02 -0500

    update sweep HPs

--- acceptability/generate_sweep.py
+++ acceptability/generate_sweep.py
@@ -101,10 +101,10 @@ space = {
             'dropout': hp.choice('dropout', [0.2, 0.5])
     }]),
     'classifier': hp.choice('classifier', [{
-            'hidden_size': hp.uniform('hidden_size', 700, 1000),
+            'hidden_size': hp.uniform('hidden_size', 30, 300),
             'embedding_size': hp.uniform('embedding_size', 200, 600),
             'learning_rate': hp.uniform('learning_rate', -5, -3),
-            'num_layers': hp.uniform('num_layers', 2, 5),
+            'num_layers': hp.uniform('num_layers', 1, 2),
             'encoding_size': hp.uniform('encoding_size', 300, 1200),
             'encoder_num_layers': hp.uniform('encoder_num_layers', 1, 5),
             'dropout': hp.choice('dropout', [0.2, 0.5])
